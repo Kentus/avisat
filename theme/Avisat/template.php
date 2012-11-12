e013ca1a5bec2d9518ddcac40d0a1cf7055f8713
Mihai Asandulesei <mihai_asandulesei@yahoo.com>
2012-11-13 01:53:27 +0200

    template

--- theme/Avisat/template.php
+++ theme/Avisat/template.php
@@ -1,12 +1,4 @@
-<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
-/****************************************************
-*
-* @File: 			template.php
-* @Package:		GetSimple
-* @Action:		Avisat theme for GetSimple CMS
-*
-*****************************************************/
-?>
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,6 +11,42 @@
 
 </head>
 <body id="<?php get_page_slug(); ?>" >
+
+	<div class="wrapper">
+    	<div class="sus">
+        </div>
+        
+        <div class="stanga">
+        	<div class="logo">
+            </div>
+            <div class="descriere">
+            	<?php get_component('descriere'); ?>
+            </div>
+            <ul>
+            	<?php get_navigation(return_page_slug()); ?>
+            </ul>
+            <div class="jos">
+           		<?php get_component('contact'); ?>
+            </div>	
+        </div>
+        
+        <div class="dreapta">
+        
+        	<div class="dreapta1">
+            </div>
+            <div class="dreapta2">
+                <div class="dreapta3">
+                </div>
+                
+            </div>
+            
+            <div class="social">
+            	<?php get_component('social'); ?>
+            </div>
+        </div>
+        <div class="galerie">
+        </div>
+    </div>
 
 </body>
 </html>
