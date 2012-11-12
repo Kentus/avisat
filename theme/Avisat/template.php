3493323c514f45efbb6ed15a50dae3f1fa5f036f
Mihai Asandulesei <mihai_asandulesei@yahoo.com>
2012-11-13 01:33:35 +0200

    Template

--- theme/Avisat/template.php
+++ theme/Avisat/template.php
@@ -1,1 +1,24 @@
-salut heck
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File: 			template.php
+* @Package:		GetSimple
+* @Action:		Avisat theme for GetSimple CMS
+*
+*****************************************************/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<!-- Site Title -->
+	<title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
+	<?php get_header(); ?>
+	<meta name="robots" content="index, follow" />
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/style.css" media="all" />
+
+</head>
+<body id="<?php get_page_slug(); ?>" >
+
+</body>
+</html>
