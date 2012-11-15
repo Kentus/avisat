bc4b60b0aebe26cd82b51614bf6934d7c73f3500
Radu Bardeanu <radu.bardeanu@yahoo.com>
2012-11-16 00:09:14 +0200

    master@work

--- theme/Avisat/template.php
+++ theme/Avisat/template.php
@@ -2,25 +2,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-	<!-- Site Title -->
 	<title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
 	<?php get_header(); ?>
 	<meta name="robots" content="index, follow" />
 	<link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/style.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/style1.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/<?php get_page_slug(); ?>.css" media="all" />
 
 </head>
-<body id="<?php get_page_slug(); ?>" >
+<body id="<?php get_page_slug(); ?>">
 
 	<div class="wrapper">
     	<div class="sus">
         </div>
         
         <div class="stanga">
-        	<img class="gradient" src="<?php get_theme_url(); ?>/images/stanga_gradient.png" alt>
         	<div class="logo">
-            	<img src="<?php get_theme_url(); ?>/images/logo.png" alt>
+            	<a href="index.php"><img src="<?php get_theme_url(); ?>/images/logo.png" alt></a>
             </div>
             <div class="descriere">
             	<?php get_component('descriere'); ?>
@@ -32,17 +29,23 @@
            		<?php get_component('contact'); ?>
             </div>	
         </div>
-        
+
         <div class="dreapta">
-        		<?php get_page_content();?>
-                </div>
-            
-            <div class="social">
-            	<?php get_component('social'); ?>
+	        <div class="dreapta1">
+	        	<?php get_page_content();?>
+	        </div>
+	        <div class="dreapta2">
+	           	<?php get_component(return_page_slug().'_dreapta2'); ?>
+			</div>
+			<div class="dreapta3">
+	           	<?php get_component(return_page_slug().'_dreapta3'); ?>
+			</div>
+	        <div class="social">
+	           	<?php get_component('social'); ?>
             </div>
         </div>
-        <div class="galerie">
-        </div>
+    </div>
+    <div class="galerie">
     </div>
 
 </body>
