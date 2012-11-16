99f86b0328e65190ce7c56ac92477ec72ac007be
Radu Bardeanu <radu.bardeanu@yahoo.com>
2012-11-16 10:00:59 +0200

    heck

--- theme/Avisat/template.php
+++ theme/Avisat/template.php
@@ -1,4 +1,23 @@
 <?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }?>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$nume = $_POST['nume'];
+	$email = $_POST['email'];
+	$subiect = $_POST['subiect'];
+	$mesaj = $_POST['mesaj'];
+	
+	$message = '';
+	$message .= '<p><b>Nume:</b> '.$nume.'</p>';
+	$message .= '<p><b>E-mail:</b> '.$email.'</p>';
+	$message .= '<p><b>Subiect:</b> '.$subiect.'</p>';
+	$message .= '<p><b>E-mail:</b> '.$email.'</p>';
+	$message .= '<p><b>Mesaj:</b><br />'.$subiect.'</p>';
+
+	mail('hecaru2004@yahoo.com', 'Mesaj din site ('.$subiect.')', $message, 'From: '.$email);
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +26,16 @@
 	<meta name="robots" content="index, follow" />
 	<link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/style.css" media="all" />
     <link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/<?php get_page_slug(); ?>.css" media="all" />
-
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/lib/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/jquery.fancybox.js?v=2.1.3"></script>
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/source/jquery.fancybox.css?v=2.1.2" media="screen" />
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" />
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+	<script type="text/javascript">$(document).ready(function() { $('.fancybox').fancybox(); });</script>
 </head>
 <body id="<?php get_page_slug(); ?>">
 
