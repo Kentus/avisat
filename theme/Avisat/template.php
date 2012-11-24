14f7bf8560371ef814ed36f39dfdf4698a10956e
Mihai Asandulesei <mihai_asandulesei@yahoo.com>
2012-11-25 00:30:01 +0200

    last ed

--- theme/Avisat/template.php
+++ theme/Avisat/template.php
@@ -22,20 +22,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html>
 <head>
 	<title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
-	<?php get_header(); ?>
+	<?php get_header(); ?>    
 	<meta name="robots" content="index, follow" />
-	<link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/style.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/<?php get_page_slug(); ?>.css" media="all" />
-	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/lib/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
-	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/jquery.fancybox.js?v=2.1.3"></script>
-	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/source/jquery.fancybox.css?v=2.1.2" media="screen" />
-	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
-	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
-	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" />
-	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
-	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
-	<script type="text/javascript">$(document).ready(function() { $('.fancybox').fancybox(); });</script>
+    <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/1024x760/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/1024x760/<?php get_page_slug(); ?>.css"/>
+  	<link rel="stylesheet" type="text/css" media="screen and (min-width: 1330px)" href="<?php get_theme_url(); ?>/1360x768/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1330px)" href="<?php get_theme_url(); ?>/1360x768/<?php get_page_slug(); ?>.css" />
+	<script type="text/javascript" src="<?php get_theme_url() ?>/jquery/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="<?php get_theme_url() ?>/jquery/jquery.mousewheel-3.0.6.pack.js"></script>
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/jquery.fancybox.js?v=2.1.3"></script>
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/jquery.fancybox.css?v=2.1.2" media="screen" />
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+	<link rel="stylesheet" type="text/css" href="<?php get_theme_url() ?>/fancybox/helpers/jquery.fancybox-thumbs.css?v=1.0.7" />
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+	<script type="text/javascript" src="<?php get_theme_url() ?>/fancybox/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+	<script type="text/javascript">
+		$(document).ready(function() { 
+			$('.fancybox').fancybox();
+		});
+		var $enable = true;
+		function next() {
+			if ($enable == false) return;
+			$enable = false;
+			$x = $('.galerie_container').position().left + $('.galerie_container a').length * 126 + 20 - 590;
+			$('.galerie_container').animate({left: '-=' + Math.min($x, 590)}, 1500, null, function() {$enable = true;});
+		}
+		function prev() {
+			if ($enable == false) return;
+			$enable = false;
+			$x = -$('.galerie_container').position().left;
+			$('.galerie_container').animate({left: '+=' + Math.min($x, 590)}, 1500, null, function() {$enable = true;});
+		}
+	</script>
+
 </head>
 <body id="<?php get_page_slug(); ?>">
 
@@ -74,6 +94,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </div>
     <div class="galerie">
+		<p>
+		</p>
+		<a class="galerie_prev" href="#" onclick="return prev();"></a>
+		<a class="galerie_next" href="#" onclick="return next();"></a>
+		<div class="galerie_wrapper">
+			<div class="galerie_container">
+<?php
+
+if ($handle = opendir('galerie')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry[0] != '.')
+		if( $entry != 'photothumb.db')
+        	echo '<a class="fancybox" href="galerie/'.$entry.'" data-fancybox-group="galerie" title=""><img src="galerie/'.$entry.'" alt /></a>';
+    }
+    closedir($handle);
+}
+
+?>
+			</div>
+		</div>
     </div>
 
 </body>
